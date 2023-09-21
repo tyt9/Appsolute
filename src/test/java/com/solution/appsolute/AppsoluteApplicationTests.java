031824938cdc7d7d4cdca47c95c1cbeea403871b
soo1915 <soo1915@naver.com>
2023-09-21 19:51:38 +0900

    Feat : jpa로 findAll 시도해보기(현재 상태는 실패)
    MailRepository에서 쿼리문 작성 후 실행 확인했는데, 쿼리 결과도 나오지 않았음.
    jpa에서 카멜 표현식이 인식되지 않아서 오류가 생기는 것으로 확인됨.
    추후 개선 방안이 나오지 않으면 MyBatis 혹은 jdbcTemplate로 변경해야 할 것으로 예상됨.

--- src/test/java/com/solution/appsolute/AppsoluteApplicationTests.java
+++ src/test/java/com/solution/appsolute/AppsoluteApplicationTests.java
@@ -1,19 +1,13 @@
 package com.solution.appsolute;
 
-import com.solution.appsolute.approval.dao.ApprovalDao;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppsoluteApplicationTests {
 
-	@Autowired
-	private ApprovalDao approvalDao;
-
 	@Test
 	void contextLoads() {
-		System.out.println(approvalDao.approvalOne(67));
 	}
 
 }
