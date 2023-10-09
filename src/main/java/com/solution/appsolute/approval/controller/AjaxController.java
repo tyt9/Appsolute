7afbcd8368fb8e3e9aade1e53100af0124ed5818
tyt9 <tythy9@gmail.com>
2023-10-09 23:01:07 +0900

    addPerson page complete...

--- src/main/java/com/solution/appsolute/approval/controller/AjaxController.java
+++ src/main/java/com/solution/appsolute/approval/controller/AjaxController.java
@@ -2,13 +2,13 @@ package com.solution.appsolute.approval.controller;
 
 import com.solution.appsolute.approval.dao.ApprovalDao;
 import com.solution.appsolute.approval.dto.Basic;
-import com.solution.appsolute.approval.dto.ExpenseReport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,10 +19,21 @@ public class AjaxController {
     private ApprovalDao approvalDao;
 
     @RequestMapping(value = "/purchaseBasic",method = RequestMethod.POST)
-    public int purchaseBasic(@RequestBody Basic basic){
+    public long purchaseBasic(@RequestBody Basic basic){
         System.out.println("------------------"+basic);
         approvalDao.insertApp(basic);
-        return 0;
+        System.out.println("------------------22"+basic);
+        return basic.getApprovalNum();
     }
 
+    @RequestMapping(value = "/addPerson", method = RequestMethod.POST)
+    public int addPerson(@RequestBody Map<String, List<Integer>> reqData){
+        System.out.println("****************"+reqData.get("empNoData"));
+        List<Integer> empNoData = reqData.get("empNoData");
+        System.out.println(empNoData.get(0));
+        System.out.println(empNoData.get(1));
+        System.out.println(empNoData.get(2));
+
+        return 0;
+    }
 }
--- src/main/java/com/solution/appsolute/approval/dto/ApprovalContentDetail.java
+++
@@ -1,5 +0,0 @@
-package com.solution.appsolute.approval.dto;
-
-public class ApprovalContentDetail {
-    private int appContentNum;
-}
