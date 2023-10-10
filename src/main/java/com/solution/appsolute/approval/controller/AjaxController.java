8638a6c37db8ddc75961cecada568bbbf869b506
tyt9 <tythy9@gmail.com>
2023-10-10 17:38:33 +0900

    addDetailVO 구성 잘못햇내요...

--- src/main/java/com/solution/appsolute/approval/controller/AjaxController.java
+++ src/main/java/com/solution/appsolute/approval/controller/AjaxController.java
@@ -1,12 +1,11 @@
 package com.solution.appsolute.approval.controller;
 
 import com.solution.appsolute.approval.dao.ApprovalDao;
+import com.solution.appsolute.approval.dto.AddDetailVO;
+import com.solution.appsolute.approval.dto.AddPersonVO;
 import com.solution.appsolute.approval.dto.Basic;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -30,10 +29,22 @@ public class AjaxController {
     public int addPerson(@RequestBody Map<String, List<Integer>> reqData){
         System.out.println("****************"+reqData.get("empNoData"));
         List<Integer> empNoData = reqData.get("empNoData");
-        System.out.println(empNoData.get(0));
-        System.out.println(empNoData.get(1));
-        System.out.println(empNoData.get(2));
+        AddPersonVO addPersonVO = new AddPersonVO();
+        for (int i = 1; i < empNoData.size(); i++) {
+            System.out.println("itar > "+empNoData.get(i));
+            addPersonVO.setEmpCheckNum(empNoData.get(i));
+            addPersonVO.setSequence(i);
+            addPersonVO.setApprovalNum(empNoData.get(0));
+            System.out.println(addPersonVO+"--------"+i);
+            approvalDao.addPerson(addPersonVO);
+        }
 
+        return 0;
+    }
+
+    @RequestMapping(value = "/addDetail", method = RequestMethod.POST)
+    public int addDetail(@RequestBody AddDetailVO addDetailVO){
+        System.out.println(addDetailVO); //VO 이렇게 만들면 안되내요...
         return 0;
     }
 }
--- src/main/java/com/solution/appsolute/approval/dao/ApprovalDao.java
+++ src/main/java/com/solution/appsolute/approval/dao/ApprovalDao.java
@@ -1,9 +1,6 @@
 package com.solution.appsolute.approval.dao;
 
-import com.solution.appsolute.approval.dto.Approval;
-import com.solution.appsolute.approval.dto.Basic;
-import com.solution.appsolute.approval.dto.Dept;
-import com.solution.appsolute.approval.dto.Employee;
+import com.solution.appsolute.approval.dto.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -16,4 +13,6 @@ public interface ApprovalDao {
     public List<Dept> deptList();
     public List<Employee> empList();
     public void insertApp(Basic basic);
+
+    public void addPerson(AddPersonVO addPersonVO);
 }
